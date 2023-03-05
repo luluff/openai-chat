99e63964d6c284c63e1dcdeee0ff00acccb151eb
luluff <1015811121@qq.com>
2023-03-05 20:37:31 +0800

    init

---
+++ go.mod
@@ -0,0 +1,22 @@
+module ct
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.7.0
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/sashabaranov/go-openai v1.4.1
+	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/testify v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	openai "github.com/sashabaranov/go-openai"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	openAIClient *openai.Client
+	log          *logrus.Logger
+)
+
+func main() {
+	initLog()
+	openAIClient = openai.NewClient("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")
+	r := gin.Default()
+	r.LoadHTMLGlob("tmpl/*")
+	r.Static("/static", "static")
+	r.GET("/chat", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	r.GET("/chat/get", func(c *gin.Context) {
+		msg := c.Query("msg")
+		if strings.TrimSpace(msg) == "" {
+			c.String(http.StatusOK, "empty input")
+			return
+		}
+		log.Info(msg)
+		rsp, err := genResponse(msg)
+		if err != nil {
+			log.Error(err.Error())
+			rsp = "服务器繁忙，请稍后再试"
+		}
+		rsp = strings.TrimLeft(rsp, "\n")
+		c.String(http.StatusOK, rsp)
+	})
+	r.Run()
+}
+func genResponse(msg string) (string, error) {
+	ctx, _ := context.WithTimeout(context.TODO(), time.Second*20)
+	resp, err := openAIClient.CreateChatCompletion(
+		ctx,
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: msg,
+				},
+			},
+		},
+	)
+	if err != nil {
+		return "", err
+	}
+	return resp.Choices[0].Message.Content, nil
+}
+func initLog() {
+	// 写入文件
+	f, err := os.OpenFile("./log.txt", os.O_APPEND|os.O_CREATE, 0777)
+	if err != nil {
+		panic(err)
+	}
+	// 实例化
+	log = logrus.New()
+	// 设置输出
+	log.Out = io.MultiWriter(f, os.Stdout)
+	// 设置日志级别
+	log.SetLevel(logrus.DebugLevel)
+}
